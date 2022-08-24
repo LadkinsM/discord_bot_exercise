4bcdd7db8e0bd04651b88358f692db89da21db0c
Mauri Adkins <mauriadkins@gmail.com>
2022-08-23 22:40:12 -0500

    Initial Discord Code Added

--- markov.py
+++ markov.py
@@ -2,6 +2,9 @@
 
 import sys
 from random import choice
+import os
+import discord
+
 
 
 def open_and_read_file(filenames):
@@ -64,3 +67,21 @@ text = open_and_read_file(filenames)
 
 # Get a Markov chain
 chains = make_chains(text)
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'Successfully connected! Logged in as {client.user}.')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    # TODO: replace this with your code
+
+
+client.run(os.environ['DISCORD_TOKEN'])
